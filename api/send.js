b8ed4952d57f6a08210bc6bd2a896c24912f5bb3
Christoph Dorn <christoph@christophdorn.com>
2015-07-21 21:08:00 -0700

    working

--- api/send.js
+++ api/send.js
@@ -1,13 +1,61 @@
 
-require('org.pinf.genesis.lib').forModule(require, module, function (API, exports) {
+require('org.pinf.genesis.lib/lib/api').forModule(require, module, function (API, exports) {
 
-	exports.sendMessage = function (args) {
-		return API.Q.denodeify(function (callback) {
+	var Send = function () {
+		var self = this;
 
-console.log("Send Twillio Message:", args);
+		self.$PLComponent = "io.pinf.service.twilio/send/0";
 
-			return callback(null);
-		})();
+		self.sendMessage = function (messageOverrides) {
+
+			return API.Q.denodeify(function (callback) {
+
+				if (self.enabled === false) {
+					API.console.debug("Skip sending mandrill email message due to 'enabled === false'");
+					return callback(null);
+				}
+
+				API.ASSERT(typeof self.profile.accountSid, "string");
+				API.ASSERT(typeof self.profile.authToken, "string");
+
+				var client = API.TWILIO(self.profile.accountSid, self.profile.authToken);
+
+				var defaultMessage = API.DEEPMERGE({
+				    body: "<REPLACE>",
+				    to: "<REPLACE>",
+				    from: "<REPLACE>"
+				}, self.message || {});
+
+				var message = API.DEEPMERGE(defaultMessage, messageOverrides || {});
+
+console.log("SMS message", message);
+
+				return client.messages.create(message, function (err, result) {
+					if (err) {
+						if (!err.stack) {
+							var _err = new Error();
+							for (var name in err) {
+								_err[name] = err[name];
+							}
+							err = _err;
+						}
+						return callback(err);
+					}
+
+console.log("SMS send result", result);
+
+					return callback(null);
+				});
+			})();
+		}
+	}
+
+	exports.PLComponent = function (config, groupConfig) {
+
+		return {
+			"$io.pinf.service.twilio/send/0": API.EXTEND(true, new Send(), config)
+		};
 	}
 
 });
+
